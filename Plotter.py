8ec385fc4e327157890ce570f92f622014154441
tylerliu <tylersiqi@163.com>
2020-04-07 18:34:32 -0700

    parallelize

--- Plotter.py
+++ Plotter.py
@@ -2,12 +2,11 @@ from matplotlib import pyplot as plt
 
 from Elevator import Elevator
 from Scheduler import ExpressScheduler, MinMaxScheduler, DynamicExpressScheduler
-from tqdm import tqdm
 import numpy as np
 from multiprocessing import Pool
 
 SIM_HOURS = 4
-pool = Pool()
+NUM_REPEAT = 4
 
 def simulate(elevator):
     for _ in range(SIM_HOURS * 3600):
@@ -21,11 +20,16 @@ def simulate(elevator):
     return stat1
 
 
-def elevator_to_stat(stats, **elevator_args):
-    for _ in range(4):
-        stat = simulate(Elevator(**elevator_args))
-        for k in stats.keys():
-            stats[k].append(stat[k])
+def get_sets(elevator_args):
+    return simulate(Elevator(**elevator_args))
+
+def to_stats(list_of_stat):
+    stats = {k : [] for k in list_of_stat[0]}
+    for k in stats:
+        for l in list_of_stat:
+            stats[k].append(l[k])
+
+    return stats
 
 
 def plot_metric(metric, units, name=None, log=False):
@@ -51,24 +55,41 @@ def pack_and_sort(metrics):
     for k in metrics:
         metrics[k] = metrics[k][sort_key]
 
-if __name__ == '__main__':
-    y_dynamic_express = {'Longest_queue': [], 'Throughput': [], 'Turnaround': [], "Total_Load": []}
-    y_express = {'Longest_queue': [], 'Throughput': [], 'Turnaround': [], "Total_Load": []}
-    y_minmax = {'Longest_queue': [], 'Throughput': [], 'Turnaround': [], "Total_Load": []}
-    y_minmax_always = {'Longest_queue': [], 'Throughput': [], 'Turnaround': [], "Total_Load": []}
-    y_minmax_full = {'Longest_queue': [], 'Throughput': [], 'Turnaround': [], "Total_Load": []}
 
-    for business in tqdm(range(101)):
-        elevator_to_stat(y_dynamic_express, scheduler=DynamicExpressScheduler(),
-                         floors=7, p=business / 1000, capacity=2, initial_count=0)
-        elevator_to_stat(y_express, scheduler=ExpressScheduler(),
-                         floors=7, p=business / 1000, capacity=2, initial_count=0)
-        elevator_to_stat(y_minmax, scheduler=MinMaxScheduler(),
-                         floors=7, p=business / 1000, capacity=2, initial_count=0)
-        elevator_to_stat(y_minmax_always, scheduler=MinMaxScheduler(),
-                         floors=7, p=business / 1000, capacity=2, initial_count=0, pressing_up='always')
-        elevator_to_stat(y_minmax_full, scheduler=MinMaxScheduler(),
-                         floors=7, p=business / 1000, capacity=2, initial_count=0, pressing_up='when_full')
+if __name__ == '__main__':
+    pool = Pool()
+
+    dy_list = pool.map(get_sets, [{'scheduler': DynamicExpressScheduler(),
+                         'floors': 7, 'p': business / 1000, 'capacity': 2, 'initial_count': 0}
+                            for business in range(101)] * NUM_REPEAT)
+    y_dynamic_express = to_stats(dy_list)
+    print('dynamic_express done')
+
+    e_list = pool.map(get_sets, [{'scheduler': ExpressScheduler(),
+                         'floors': 7, 'p': business / 1000, 'capacity': 2, 'initial_count': 0}
+                                  for business in range(101)] * NUM_REPEAT)
+    y_express = to_stats(e_list)
+    print('express done')
+
+    mm_list = pool.map(get_sets, [{'scheduler': MinMaxScheduler(),
+                                  'floors': 7, 'p': business / 1000, 'capacity': 2, 'initial_count': 0}
+                                 for business in range(101)] * NUM_REPEAT)
+    y_minmax = to_stats(mm_list)
+    print('minmax done')
+
+    mma_list = pool.map(get_sets, [{'scheduler': MinMaxScheduler(),
+                                   'floors': 7, 'p': business / 1000, 'capacity': 2, 'initial_count': 0,
+                                    'pressing_up': 'always'}
+                                  for business in range(101)] * NUM_REPEAT)
+    y_minmax_always = to_stats(mma_list)
+    print('minmax always done')
+
+    mmf_list = pool.map(get_sets, [{'scheduler': MinMaxScheduler(),
+                                   'floors': 7, 'p': business / 1000, 'capacity': 2, 'initial_count': 0,
+                                    'pressing_up': 'when_full'}
+                                   for business in range(101)] * NUM_REPEAT)
+    y_minmax_full = to_stats(mmf_list)
+    print('minmax full done')
 
     pack_and_sort(y_dynamic_express)
     pack_and_sort(y_express)
